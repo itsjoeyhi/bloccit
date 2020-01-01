9727a2cf7344b591244297c1601434cdccc2364d
Joseph <joseph@Josephs-MacBook-Pro.local>
2019-12-31 23:39:28 -0500

    route/controllers/view chkpoint done

--- src/routes/static.js
+++ src/routes/static.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.send("Welcome to Bloccit");
-});
+const staticController = require("../controllers/staticController");
 
 module.exports = router;
+router.get("/", staticController.index);
